3944df7d0fa9dade9cb29b14a3f251052500ac74
krunaladdweb <48708155+krunaladdweb@users.noreply.github.com>
2019-11-26 13:07:35 +0530

    Update provider.tf

--- eks-terraform/provider.tf
+++ eks-terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAQXHG76K372QWT3Y3"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
